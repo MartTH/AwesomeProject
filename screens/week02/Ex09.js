4cd7e0be6c60be1d9698c3911e26cc161d114bb8
MartTH <118419696+MartTH@users.noreply.github.com>
2023-01-18 22:20:35 +0700

    Add files via upload

---
+++ screens/week02/Ex09.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export default function Ex09() {
+  return (
+    <View style={{ flex : 1 , flexDirection : 'row', justifyContent : 'space-between'}}>
+        <View style={{justifyContent : 'space-between', alignItems : 'flex-start'}}>
+            <View style={{ backgroundColor : '#50E3C2' , height : 100, width : 100  }}></View>
+            <View style={{ backgroundColor : '#50E3C2' , height : 100, width : 100  }}></View>
+            <View style={{ backgroundColor : '#50E3C2' , height : 100, width : 100  }}></View>
+        </View>
+        <View style={{justifyContent : 'space-between', alignItems : 'center'}}>
+            <View style={{ backgroundColor : '#4A90E2' , height : 100, width : 100  }}></View>
+            <View style={{ backgroundColor : '#4A90E2' , height : 100, width : 100  }}></View>
+            <View style={{ backgroundColor : '#4A90E2' , height : 100, width : 100  }}></View>
+        </View>
+        <View style={{justifyContent : 'space-between', alignItems : 'flex-end'}}>
+            <View style={{ backgroundColor : '#9013FE' , height : 100, width : 100  }}></View> 
+            <View style={{ backgroundColor : '#9013FE' , height : 100, width : 100  }}></View>
+            <View style={{ backgroundColor : '#9013FE' , height : 100, width : 100  }}></View>  
+        </View> 
+    </View>
+  );
+}
