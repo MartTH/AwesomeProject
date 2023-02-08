042058b5c00be614639496464279d62f78490192
MartTH <118419696+MartTH@users.noreply.github.com>
2023-02-09 01:03:07 +0700

    week7

--- screens/week02/EX06.js
+++ screens/week02/EX06.js
@@ -3,16 +3,16 @@ import React from 'react';
 import { Button } from 'react-native';
 import { View } from 'react-native';
 
-export default function Ex05() {
+export default function Ex06() {
   const navigation = useNavigation();
   return (
     <View style={{ flex: 1 }}>
-    <View style={{ flex : 1, flexDirection : 'row', justifyContent : 'center', alignItems : 'center'}}>    
+    <View style={{ flex : 1, flexDirection : 'column', justifyContent : 'center', alignItems : 'center'}}>    
       <View style={{ backgroundColor : '#50E3C2' , height : 100, width : 100  }}></View>
       <View style={{ backgroundColor : '#4A90E2' , height : 100, width : 100  }}></View>
       <View style={{ backgroundColor : '#9013FE' , height : 100, width : 100  }}></View>
     </View>
-    <Button title="Next" onPress={() => navigation.navigate("Ex06")} />
+    <Button title="Next" onPress={() => navigation.navigate("Ex07")} />
     </View>
   );
 }
